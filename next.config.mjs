ed496f7f883edeacf70b777189f56f693d45d823
Mounssif Koumila <m.koumila@void.fr>
2024-02-08 22:18:17 +0100

    fix: fixing images by removing next/image optimization (wip)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["res.cloudinary.com"] },
+  images: { domains: ["res.cloudinary.com"], unoptimized: true },
   output: "export",
 };
 
