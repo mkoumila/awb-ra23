57f40ea5011e3be036df7d4c07f1f47d4a82987c
Mustapha BKL <m.boukali@void.fr>
2024-04-05 10:48:46 +0000

    feat: update htaccess

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,14 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-//const AUTH_USER= "awb-rapport"
-//const AUTH_PASS = "void@awb@rapport"
-
-const AUTH_USER= "user"
-const AUTH_PASS = "password"
+const AUTH_USER = "awb-rapport";
+const AUTH_PASS = "void@awb@rapport";
 
 // Step 1. HTTP Basic Auth Middleware for Challenge
 export function middleware(req) {
-  console.log("==========================================================================================")
-  console.log("==========================================================================================")
-  console.log("==========================================================================================")
-  console.log("==========================================================================================")
-  console.log("middlware src")
-
   if (!isAuthenticated(req)) {
-    return new NextResponse('Authentication required', {
+    return new NextResponse("Authentication required", {
       status: 401,
-      headers: { 'WWW-Authenticate': 'Basic' },
+      headers: { "WWW-Authenticate": "Basic" },
     });
   }
 
@@ -26,13 +17,16 @@ export function middleware(req) {
 
 // Step 2. Check HTTP Basic Auth header if present
 function isAuthenticated(req) {
-  const authheader = req.headers.get('authorization') || req.headers.get('Authorization');
+  const authheader =
+    req.headers.get("authorization") || req.headers.get("Authorization");
 
   if (!authheader) {
     return false;
   }
 
-  const auth = Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  const auth = Buffer.from(authheader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
   const user = auth[0];
   const pass = auth[1];
 
@@ -43,7 +37,6 @@ function isAuthenticated(req) {
   }
 }
 
-
 export const config = {
   matcher: [
     /*
@@ -53,9 +46,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    '/'
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/",
   ],
-}
-
-
+};
